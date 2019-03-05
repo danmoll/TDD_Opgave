9d7b90cac469c55ae5e10615a639fb00de7dc9a6
DanMoll <dani948d@edu.easj.dk>
2019-03-05 13:01:25 +0100

    Antalstæller kan både finde én, flere, case insensitive samt giver -1 tilbage ved null string.

---
+++ ClassLibraryString/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ClassLibraryString
+{
+    public class StringUtilities
+    {
+        public int CountOccurences(string StringToCheck, string StringToFind)
+        {
+            int countresult = -1;
+            if (StringToCheck != null)
+            {
+                countresult = StringToCheck.Count(x => Char.ToUpperInvariant(StringToFind[0]) == Char.ToUpperInvariant(x));
+            }
+            return countresult;
+        }
+    }
+}
--- TDD_Opgave_Test/UnitTest1.cs
+++ TDD_Opgave_Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryString;
 
 namespace TDD_Opgave_Test
 {
@@ -6,8 +7,71 @@ namespace TDD_Opgave_Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSingleOccurrence()
         {
+            //Arrange
+            var stringToCheck = "mysterious";
+            var stringToFind = "y";
+            var expectedResult = 1;
+
+            var classUnderTest = new StringUtilities();
+
+            //Act
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethodMultipleOccurences()
+        {
+            //Arrange
+            var stringToCheck = "mysterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+
+            var classUnderTest = new StringUtilities();
+
+            //Act
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethodCaseInsensitive()
+        {
+            //Arrange
+            var stringToCheck = "mySterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+
+            var classUnderTest = new StringUtilities();
+
+            //Act
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethodNull()
+        {
+            //Arrange
+            string stringToCheck = null;
+            var stringToFind = "s";
+            var expectedResult = -1;
+
+            var classUnderTest = new StringUtilities();
+
+            //Act
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
